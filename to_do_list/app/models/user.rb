c6ecc7c1f7237e76478e2cb6e0e334ba37b989e4
GettyGertie <gettyorawo@gmail.com>
2016-08-02 12:15:47 +0300

    Complete remenber me

--- to_do_list/app/models/user.rb
+++ to_do_list/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   validates :name, presence: true, length: {maximum:20}, uniqueness: { case_sensitive: false  }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 30},
@@ -24,6 +25,11 @@ class User < ApplicationRecord
   end
 
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
