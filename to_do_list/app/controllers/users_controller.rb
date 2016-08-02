b7cc60732d341d2c0c953506d835abccf2c85465
GettyGertie <gettyorawo@gmail.com>
2016-08-02 23:08:09 +0300

    Experience tests errors

--- to_do_list/app/controllers/users_controller.rb
+++ to_do_list/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -24,16 +27,35 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.update_attributes(params[:user])
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
 private
 
 def user_params
   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+end
+
+def logged_in_user
+  unless logged_in?
+    store_location
+    flash[:danger] = "Please login first!"
+    redirect_to login_path
+  end
+end
+
+def correct_user
+  @user = User.find(params[:id])
+  redirect_to(root_url) unless current_user?(@user)
 end
 end
--- to_do_list/app/models/user.rb
+++ to_do_list/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 30},
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: {minimum: 5}
+  validates :password, presence: true, length: {minimum: 5}, allow_blank: true
   has_secure_password
 
   def User.digest(string)
