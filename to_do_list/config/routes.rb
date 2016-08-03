af019872e3cb86e2c0ef57ad9a1b52c407d3b266
GettyGertie <gettyorawo@gmail.com>
2016-08-03 18:53:15 +0300

    Complete password resets

--- to_do_list/config/routes.rb
+++ to_do_list/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'signup' => 'users#new'
 
   get 'contact' => 'pages#contact'
@@ -17,4 +21,6 @@ Rails.application.routes.draw do
   resources :users
 
   resources :account_activations, only: [:edit]
+
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
