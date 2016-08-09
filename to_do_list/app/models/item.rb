6a67a282250de3b69a4dbc6921cb1d20ec363007
GettyGertie <gettyorawo@gmail.com>
2016-08-09 07:20:36 +0300

    Fix enum issue

--- to_do_list/app/models/item.rb
+++ to_do_list/app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :card
+
   enum condition{ "public": 0,  "private": 1 }
-  enum status{ "incomplete": 0, "done": 1 }
+
+  enum status{ "incomplete": 0, "done" }
 end
