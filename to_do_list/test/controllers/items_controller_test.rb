83954bc914ab8f911837fc55f5fb89592f3cd9f8
GettyGertie <gettyorawo@gmail.com>
2016-08-09 22:24:17 +0300

    Add integration tests

--- to_do_list/test/controllers/items_controller_test.rb
+++ to_do_list/test/controllers/items_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get items_new_url
+    get new_item_url
     assert_response :success
   end
 
